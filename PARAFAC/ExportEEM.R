11ef6ffbc2d76f6a55f171bde9300ecf0810e845
Richard LaBrie <Richard LaBrie>
2017-04-26 20:59:43 -0400

    Version fonctionnelle

--- PARAFAC/ExportEEM.R
+++ PARAFAC/ExportEEM.R
@@ -7,19 +7,24 @@ ExportEEM <- function(cube,cut = c(0,0,0))
 	EEMCut = 0
 	if(cut[1]>0)
 	{
-		ExCut = c(1:cut[1])
+	  ExCut = c(1,cut[1])
+		cube[[1]] <- cube[[1]][-ExCut,,]
+		cube[[3]] <- cube[[3]][-ExCut]
 	}
 	if(cut[2]>0)
 	{
 		EmCut = c(1:cut[2])
+		cube[[1]] <- cube[[1]][,-EmCut,]
+		cube[[4]] <- cube[[4]][-EmCut]
 	}
 	if(cut[3]>0)
 	{
 		EEMCut = c(1:cut[3])
+		cube[[1]] <- cube[[1]][,,-EEMCut]
 	}
 	setwd(getwd())
-	write.table(cube[[1]][-ExCut,-EmCut,-EEMCut],"DataCube.txt")
+	write.table(cube[[1]],"DataCube.txt")
 	write.table(unlist(cube[[2]]),"DataName.txt")
-	write.table(cube[[3]][-ExCut],"ExcitationSequence.txt")
-	write.table(cube[[4]][-EmCut],"EmissionSequence.txt")
+	write.table(cube[[3]],"ExcitationSequence.txt")
+	write.table(cube[[4]],"EmissionSequence.txt")
 }
